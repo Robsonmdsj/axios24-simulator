e787f4cd958ffaa1bfd4b43ff1279436a6ac3ee4
Robsonmdsj <robson_mdsj@hotmail.com>
2026-07-02 14:48:16 -0300

    estrutura

---
+++ js/app.js
@@ -0,0 +1,85 @@
+const mixer = document.getElementById("mixer");
+
+for(let i=1;i<=24;i++){
+
+    mixer.innerHTML += criarCanal(i);
+
+}
+
+mixer.innerHTML += criarMaster();
+
+function criarCanal(numero){
+
+return`
+
+<div class="channel">
+
+<div class="display">
+
+CH ${numero}
+
+</div>
+
+<div class="meter">
+
+<div class="level"></div>
+
+</div>
+
+<button>Mute</button>
+
+<button>Solo</button>
+
+<input type="range"
+
+min="0"
+
+max="100"
+
+value="75"
+
+class="fader"
+
+orient="vertical">
+
+</div>
+
+`;
+
+}
+
+function criarMaster(){
+
+return`
+
+<div class="master">
+
+<div class="display">
+
+MASTER
+
+</div>
+
+<div class="meter">
+
+<div class="level"></div>
+
+</div>
+
+<input type="range"
+
+min="0"
+
+max="100"
+
+value="80"
+
+class="fader"
+
+orient="vertical">
+
+</div>
+
+`;
+
+}
